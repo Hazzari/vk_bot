2c044aebfe857d0604083a7af6ce17a0cd230df8
Aleksan <Dev.aleksan@gmail.com>
2020-05-06 02:14:00 +0300

    add documentation

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Бот работает на стророне клиента и слушает события с серевера
+Бот умеет только отправлять эхо сообщения на текст пользователя.
+
+"""
 
 # imports
 import os
@@ -7,7 +12,6 @@ from random import randint
 
 import vk_api
 from dotenv import load_dotenv
-# import vk_api.bot_longpoll
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 # # Настройка окружения
@@ -26,18 +30,27 @@ class Bot:
         self.long_poller = VkBotLongPoll(self.vk, group_id=self.group_id)
 
     def run(self):
+        """
+        Получаем сообщения от сервера
+        :return: None
+        """
         for event in self.long_poller.listen():
-            # print('Получено событие:')
             try:
                 self.on_event(event)
             except Exception as err:
                 print(err)
 
     def on_event(self, event):
+        """
+        Обрабатываем полученный event
+        :param event: Обьект который передает сайт VK
+        :return: None
+        """
         if event.type == VkBotEventType.MESSAGE_NEW:
+            response_text = f'Я вас понял, вы написали: "{event.obj.message["text"]}". Теперь мы обработаем ваш запрос!'
             self.api.messages.send(
                 random_id=randint(1, 2 ** 20),
-                message=event.obj.message['text'],
+                message=response_text,
                 peer_id=event.message.peer_id,
             )
         else:
